546901487dc2db2d51e282cbed935c91eb6c7ea2
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-08-19 09:12:10 +0200

    pdfsharp

--- ProductivityTools.PDFCommentsExtractor.Manager/Class1.cs
+++ ProductivityTools.PDFCommentsExtractor.Manager/Class1.cs
@@ -1,7 +1,65 @@
-﻿namespace ProductivityTools.PDFCommentsExtractor.Manager
+﻿
+
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.Content;
+using PdfSharp.Pdf.Content.Objects;
+using System.IO.Pipes;
+
+namespace ProductivityTools.PDFCommentsExtractor.Manager
 {
     public class Class1
     {
+        public IEnumerable<string> ExtractText(CObject cObject)
+        {
+            if (cObject is COperator)
+            {
+                var cOperator = cObject as COperator;
+                if (cOperator.OpCode.Name == OpCodeName.Tj.ToString() ||
+                    cOperator.OpCode.Name == OpCodeName.TJ.ToString())
+                {
+                    foreach (var cOperand in cOperator.Operands)
+                        foreach (var txt in ExtractText(cOperand))
+                            yield return txt;
+                }
+            }
+            else if (cObject is CSequence)
+            {
+                var cSequence = cObject as CSequence;
+                foreach (var element in cSequence)
+                    foreach (var txt in ExtractText(element))
+                        yield return txt;
+            }
+            else if (cObject is CString)
+            {
+                var cString = cObject as CString;
+                yield return cString.Value;
+            }
+        }
 
+        public void Read()
+        {
+            //(
+            string path = @"D:\Trash\2024_33.pdf";
+            PdfSharp.Pdf.PdfDocument doc = PdfSharp.Pdf.IO.PdfReader.Open(path);
+            for (int i = 0; i < doc.PageCount; i++)
+            {
+                var page = doc.Pages[i];
+                var pagec = page.Contents;
+                var annotation = page.Annotations;
+                var content = ContentReader.ReadContent(page);
+                var text = ExtractText(content);
+                string result = string.Empty;
+                foreach(var t in text)
+                {
+                    result += t;
+                }
+
+                for (int j = 0; j < annotation.Count; j++)
+                {
+                    var x = annotation[j].Contents;
+                    Console.Write(annotation[j].ToString());
+                }
+            }
+        }
     }
 }
---
+++ ProductivityTools.PDFCommentsExtractor.PdfRunner/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProductivityTools.PDFCommentsExtractor.Manager;
+
+Console.WriteLine("Hello, World!");
+Class1 c = new Class1();
+c.Read();
